c913b42d5bc2e01f79d3f6e5755ccfe5180465c5
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-01-05 00:59:47 -0500

    Remove old debugging statements

--- tests/test_wigner.py
+++ tests/test_wigner.py
@@ -24,71 +24,16 @@ def test_H(eps):
     w = sf.Wigner(ell_max)
     Hnmpm = w.H(np.exp(1j * beta))
     max_error = 0.0
-    # errors = np.empty(hcalc.Hsize)
 
-    # print("Hnmpm:")
-    # print(repr(Hnmpm))
-
-    # print()
     for n in range(w.ell_max+1):
-        # print(f'Testing n={n} compared to sympy')
-
-        # print("sympy:")
-        # print([[sympy.re(sympy.N(sympyWignerD(n, mp, m, alpha, -beta, gamma).doit()))
-        #         for mp in range(-n, n+1)]
-        #        for m in range(-n, n+1)
-        #        ])
-        # print()
-        # print("spherical:")
-        # print([[ϵ(mp) * ϵ(-m) * Hnmpm[sf.WignerHindex(n, mp, m)]
-        #         for mp in range(-n, n+1)]
-        #        for m in range(-n, n+1)
-        #        ])
-
         for mp in range(-n, n+1):
             for m in range(-n, n+1):
-                sympyd = sympy.re(sympy.N(sympyWignerD(n, mp, m, alpha, -beta, gamma).doit()))
-                # myd = ϵ(mp) * ϵ(-m) * Hnmpm[sf.WignerHindex(n, mp, m)]
-                myd = ϵ(-mp) * ϵ(m) * Hnmpm[sf.WignerHindex(n, mp, m)]
+                sympyd = sympy.re(sympy.N(sympyWignerD(n, mp, m, alpha, beta, gamma).doit()))
+                myd = ϵ(mp) * ϵ(-m) * Hnmpm[sf.WignerHindex(n, mp, m)]
+                # sympyd = sympy.re(sympy.N(sympyWignerD(n, mp, m, alpha, -beta, gamma).doit()))
+                # myd = ϵ(-mp) * ϵ(m) * Hnmpm[sf.WignerHindex(n, mp, m)]
                 error = float(abs(sympyd-myd))
-                # error = float(min(abs(sympyd+myd), abs(sympyd-myd)))
-                if error >= 3*eps:
-                    print(
-                        f"Hnmpm[sf.WignerHindex({n}, {mp}, {m})]: "
-                        f"{Hnmpm.tolist()}[{sf.WignerHindex(n, mp, m)}] = "
-                        f"{Hnmpm[sf.WignerHindex(n, mp, m)]}"
-                    )
                 assert error < 3*eps, f"Testing Wigner d recursion: n={n}, m'={mp}, m={m}, sympy:{sympyd}, spherical:{myd}, error={error}"
                 max_error = max(error, max_error)
-                # errors[i] = float(min(abs(sympyd+myd), abs(sympyd-myd)))
-                # print("{:>5} {:>5} {:>5} {:24} {:24} {:24}".format(n, mp, m, float(sympyd), myd, errors[i]))
 
     # print(f"\nTesting H (Wigner d recursion): max error = {max_error}")
-
-
-# def test_WignerDRecursion_timing():
-#     import timeit
-#     import textwrap
-#     print()
-#     hcalc = HCalculator(8)
-#     for ell_max in [8, 100]:
-#         cosβ = 2*np.random.rand(2*ell_max+1, 2*ell_max+1) - 1
-#         workspace = hcalc.workspace(cosβ)
-#         size = hcalc(cosβ, workspace=workspace).size  # Run once to ensure everything is compiled
-#         number = 1000 // ell_max
-#         time = timeit.timeit('hcalc(cosβ, workspace=workspace)', number=number, globals={'hcalc': hcalc, 'cosβ': cosβ, 'workspace': workspace})
-#         print('Time for ell_max={} grid points was {}ms per call; {}ns per element'.format(100, 1_000*time/number, 1_000_000_000*time/(number*size)))
-
-
-# @pytest.mark.skipif(platform.system() == "Windows", reason="line_profiler is missing")
-# def test_WignerDRecursion_lineprofiling():
-#     from line_profiler import LineProfiler
-#     ell_max = 8
-#     hcalc = HCalculator(ell_max)
-#     cosβ = 2*np.random.rand(100, 100) - 1
-#     workspace = hcalc.workspace(cosβ)
-#     hcalc(cosβ, workspace=workspace)  # Run once to ensure everything is compiled
-#     profiler = LineProfiler(hcalc.__call__)#, _step_2, _step_3, _step_4, _step_5, _step_6)
-#     profiler.runctx('hcalc(cosβ, workspace=workspace)', {'hcalc': hcalc, 'cosβ': cosβ, 'workspace': workspace}, {})
-#     print()
-#     profiler.print_stats()
