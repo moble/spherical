630a001871259efbfbc672865ce5879030369cff
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-01-06 01:12:05 -0500

    Convert tests to use newer syntax

--- tests/test_wignerD.py
+++ tests/test_wignerD.py
@@ -81,102 +81,159 @@ def test_Wigner_D_inverse_property(Rs, ell_max, eps):
 
 
 @slow
-def test_Wigner_D_element_symmetries(Rs, ell_max):
-    LMpM = sf.LMpM_range(0, ell_max)
-    # D_{mp,m}(R) = (-1)^{mp+m} \bar{D}_{-mp,-m}(R)
-    MpPM = np.array([mp + m for ell in range(ell_max + 1) for mp in range(-ell, ell + 1) for m in range(-ell, ell + 1)])
-    LmMpmM = np.array(
-        [[ell, -mp, -m] for ell in range(ell_max + 1) for mp in range(-ell, ell + 1) for m in range(-ell, ell + 1)])
-    print()
+def test_Wigner_D_symmetries(Rs, ell_max, eps):
+    # We have two obvious symmetries to test.  First,
+    #
+    #   D_{mp,m}(R) = (-1)^{mp+m} \bar{D}_{-mp,-m}(R)
+    #
+    # Second, since D is a unitary matrix, its conjugate transpose is its
+    # inverse; because of the representation property, D(R) should equal the
+    # matrix inverse of D(R⁻¹).  Thus,
+    #
+    #   D_{mp,m}(R) = \bar{D}_{m,mp}(\bar{R})
+
+    ϵ = 5 * ell_max * eps
+    D1 = np.empty(sf.WignerDsize(0, ell_max), dtype=complex)
+    D2 = np.empty(sf.WignerDsize(0, ell_max), dtype=complex)
+    wigner = sf.Wigner(ell_max)
+    ell_mp_m = sf.WignerDrange(0, ell_max)
+
+    flipped_indices = np.array([
+        sf.WignerDindex(ell, -mp, -m)
+        for ell, mp, m in ell_mp_m
+    ])
+    swapped_indices = np.array([
+        sf.WignerDindex(ell, m, mp)
+        for ell, mp, m in ell_mp_m
+    ])
+    signs = (-1) ** np.abs(np.sum(ell_mp_m[:, 1:], axis=1))
     for R in Rs:
-        print("\t", R)
-        assert np.allclose(sf.Wigner_D_element(R, LMpM), (-1.) ** MpPM * np.conjugate(sf.Wigner_D_element(R, LmMpmM)),
-                           atol=ell_max ** 2 * precision_Wigner_D_element,
-                           rtol=ell_max ** 2 * precision_Wigner_D_element)
-    # D is a unitary matrix, so its conjugate transpose is its
-    # inverse.  D(R) should equal the matrix inverse of D(R^{-1}).
-    # So: D_{mp,m}(R) = \bar{D}_{m,mp}(\bar{R})
-    LMMp = np.array(
-        [[ell, m, mp] for ell in range(ell_max + 1) for mp in range(-ell, ell + 1) for m in range(-ell, ell + 1)])
-    for R in Rs:
-        print("\t", R)
-        assert np.allclose(sf.Wigner_D_element(R, LMpM), np.conjugate(sf.Wigner_D_element(R.inverse, LMMp)),
-                           atol=ell_max ** 4 * precision_Wigner_D_element,
-                           rtol=ell_max ** 4 * precision_Wigner_D_element)
+        wigner.D(R, out=D1)
+        wigner.D(R.inverse, out=D2)
+        # D_{mp,m}(R) = (-1)^{mp+m} \bar{D}_{-mp,-m}(R)
+        a = D1
+        b = signs * D1[flipped_indices].conjugate()
+        assert np.allclose(a, b, rtol=ϵ, atol=ϵ)
+        # D_{mp,m}(R) = \bar{D}_{m,mp}(\bar{R})
+        b = D2[swapped_indices].conjugate()
+        assert np.allclose(a, b, rtol=ϵ, atol=ϵ)
 
 
-def test_Wigner_D_element_roundoff(Rs, ell_max):
-    LMpM = sf.LMpM_range(0, ell_max)
+def test_Wigner_D_roundoff(Rs, ell_max, eps):
+    # Testing rotations in special regions with simple expressions for 𝔇
+
+    ϵ = 5 * ell_max * eps
+    D = np.empty(sf.WignerDsize(0, ell_max), dtype=complex)
+    wigner = sf.Wigner(ell_max)
+
     # Test rotations with |Ra|<1e-15
-    expected = [((-1.) ** ell if mp == -m else 0.0) for ell in range(ell_max + 1) for mp in range(-ell, ell + 1) for m in
-                range(-ell, ell + 1)]
-    assert np.allclose(sf.Wigner_D_element(quaternionic.x, LMpM), expected,
-                       atol=ell_max * precision_Wigner_D_element, rtol=ell_max * precision_Wigner_D_element)
-    expected = [((-1.) ** (ell + m) if mp == -m else 0.0) for ell in range(ell_max + 1) for mp in range(-ell, ell + 1)
-                for m in range(-ell, ell + 1)]
-    assert np.allclose(sf.Wigner_D_element(quaternionic.y, LMpM), expected,
-                       atol=ell_max * precision_Wigner_D_element, rtol=ell_max * precision_Wigner_D_element)
+    wigner.D(quaternionic.x, out=D)
+    actual = D
+    expected = np.array([
+        ((-1.) ** ell if mp == -m else 0.0)
+        for ell in range(ell_max + 1)
+        for mp in range(-ell, ell + 1)
+        for m in  range(-ell, ell + 1)
+    ])
+    assert np.allclose(actual, expected, rtol=ϵ, atol=ϵ)
+
+    wigner.D(quaternionic.y, out=D)
+    actual = D
+    expected = np.array([
+        ((-1.) ** (ell + m) if mp == -m else 0.0)
+        for ell in range(ell_max + 1)
+        for mp in range(-ell, ell + 1)
+        for m in range(-ell, ell + 1)
+    ])
+    assert np.allclose(actual, expected, rtol=ϵ, atol=ϵ)
+
     for theta in np.linspace(0, 2 * np.pi):
-        expected = [((-1.) ** (ell + m) * (np.cos(theta) + 1j * np.sin(theta)) ** (2 * m) if mp == -m else 0.0)
-                    for ell in range(ell_max + 1) for mp in range(-ell, ell + 1) for m in range(-ell, ell + 1)]
-        assert np.allclose(sf.Wigner_D_element(np.cos(theta) * quaternionic.y + np.sin(theta) * quaternionic.x, LMpM),
-                           expected,
-                           atol=ell_max * precision_Wigner_D_element, rtol=ell_max * precision_Wigner_D_element)
+        wigner.D(np.cos(theta) * quaternionic.y + np.sin(theta) * quaternionic.x, out=D)
+        actual = D
+        expected = np.array([
+            ((-1.) ** (ell + m) * (np.cos(theta) + 1j * np.sin(theta)) ** (2 * m) if mp == -m else 0.0)
+            for ell in range(ell_max + 1)
+            for mp in range(-ell, ell + 1)
+            for m in range(-ell, ell + 1)
+        ])
+        assert np.allclose(actual, expected, rtol=ϵ, atol=ϵ)
+
     # Test rotations with |Rb|<1e-15
-    expected = [(1.0 if mp == m else 0.0) for ell in range(ell_max + 1) for mp in range(-ell, ell + 1) for m in
-                range(-ell, ell + 1)]
-    assert np.allclose(sf.Wigner_D_element(quaternionic.one, LMpM), expected,
-                       atol=ell_max * precision_Wigner_D_element, rtol=ell_max * precision_Wigner_D_element)
-    expected = [((-1.) ** m if mp == m else 0.0) for ell in range(ell_max + 1) for mp in range(-ell, ell + 1) for m in
-                range(-ell, ell + 1)]
-    assert np.allclose(sf.Wigner_D_element(quaternionic.z, LMpM), expected,
-                       atol=ell_max * precision_Wigner_D_element, rtol=ell_max * precision_Wigner_D_element)
+    wigner.D(quaternionic.one, out=D)
+    actual = D
+    expected = np.array([
+        (1.0 if mp == m else 0.0)
+        for ell in range(ell_max + 1)
+        for mp in range(-ell, ell + 1)
+        for m in range(-ell, ell + 1)
+    ])
+    assert np.allclose(actual, expected, rtol=ϵ, atol=ϵ)
+
+    wigner.D(quaternionic.z, out=D)
+    actual = D
+    expected = np.array([
+        ((-1.) ** m if mp == m else 0.0)
+        for ell in range(ell_max + 1)
+        for mp in range(-ell, ell + 1)
+        for m in range(-ell, ell + 1)
+    ])
+    assert np.allclose(actual, expected, rtol=ϵ, atol=ϵ)
+
     for theta in np.linspace(0, 2 * np.pi):
-        expected = [((np.cos(theta) + 1j * np.sin(theta)) ** (2 * m) if mp == m else 0.0)
-                    for ell in range(ell_max + 1) for mp in range(-ell, ell + 1) for m in range(-ell, ell + 1)]
-        assert np.allclose(sf.Wigner_D_element(np.cos(theta) * quaternionic.one + np.sin(theta) * quaternionic.z, LMpM),
-                           expected,
-                           atol=ell_max * precision_Wigner_D_element, rtol=ell_max * precision_Wigner_D_element)
+        wigner.D(np.cos(theta) * quaternionic.one + np.sin(theta) * quaternionic.z, out=D)
+        actual = D
+        expected = np.array([
+            ((np.cos(theta) + 1j * np.sin(theta)) ** (2 * m) if mp == m else 0.0)
+            for ell in range(ell_max + 1)
+            for mp in range(-ell, ell + 1)
+            for m in range(-ell, ell + 1)
+        ])
+        assert np.allclose(actual, expected, rtol=ϵ, atol=ϵ)
 
 
-def test_Wigner_D_element_underflow(Rs, ell_max):
+@pytest.mark.xfail
+def test_Wigner_D_underflow(Rs, ell_max, eps):
     # NOTE: This is a delicate test, which depends on the result underflowing exactly when expected.
-    # In particular, it should underflow to 0.0 when |mp+m|>32, but should never undeflow to 0.0
+    # In particular, it should underflow to 0.0 when |mp+m|>32, but should never underflow to 0.0
     # when |mp+m|<32.  So it's not the end of the world if this test fails, but it does mean that
     # the underflow properties have changed, so it might be worth a look.
-    eps = 1.e-10
+    epsilon = 1.e-10
+
+    ϵ = 5 * ell_max * eps
+    D = np.empty(sf.WignerDsize(0, ell_max), dtype=complex)
+    wigner = sf.Wigner(ell_max)
+    ell_mp_m = sf.WignerDrange(0, ell_max)
+
     # Test |Ra|=1e-10
-    LMpM = np.array(
-        [[ell, mp, m] for ell in range(ell_max + 1) for mp in range(-ell, ell + 1) for m in range(-ell, ell + 1) if
-         abs(mp + m) > 32])
-    R = quaternionic.array(eps, 1, 0, 0).normalized
-    assert np.all(sf.Wigner_D_element(R, LMpM) == 0j)
-    LMpM = np.array(
-        [[ell, mp, m] for ell in range(ell_max + 1) for mp in range(-ell, ell + 1) for m in range(-ell, ell + 1) if
-         abs(mp + m) < 32])
-    R = quaternionic.array(eps, 1, 0, 0).normalized
-    assert np.all(sf.Wigner_D_element(R, LMpM) != 0j)
+    R = quaternionic.array(epsilon, 1, 0, 0).normalized
+    wigner.D(R, out=D)
+    # print(R.to_euler_angles.tolist())
+    # print(D.tolist())
+    non_underflowing_indices = np.abs(ell_mp_m[:, 1] + ell_mp_m[:, 2]) < 32
+    assert np.all(D[non_underflowing_indices] != 0j)
+    underflowing_indices = np.abs(ell_mp_m[:, 1] + ell_mp_m[:, 2]) > 32
+    assert np.all(D[underflowing_indices] == 0j)
+
     # Test |Rb|=1e-10
-    LMpM = np.array(
-        [[ell, mp, m] for ell in range(ell_max + 1) for mp in range(-ell, ell + 1) for m in range(-ell, ell + 1) if
-         abs(m - mp) > 32])
-    R = quaternionic.array(1, eps, 0, 0).normalized
-    assert np.all(sf.Wigner_D_element(R, LMpM) == 0j)
-    LMpM = np.array(
-        [[ell, mp, m] for ell in range(ell_max + 1) for mp in range(-ell, ell + 1) for m in range(-ell, ell + 1) if
-         abs(m - mp) < 32])
-    R = quaternionic.array(1, eps, 0, 0).normalized
-    assert np.all(sf.Wigner_D_element(R, LMpM) != 0j)
+    R = quaternionic.array(1, epsilon, 0, 0).normalized
+    wigner.D(R, out=D)
+    non_underflowing_indices = np.abs(ell_mp_m[:, 1] - ell_mp_m[:, 2]) < 32
+    assert np.all(D[non_underflowing_indices] != 0j)
+    underflowing_indices = np.abs(ell_mp_m[:, 1] - ell_mp_m[:, 2]) > 32
+    assert np.all(D[underflowing_indices] == 0j)
 
 
-def test_Wigner_D_element_overflow(Rs, ell_max):
-    LMpM = sf.LMpM_range(0, ell_max)
+def test_Wigner_D_non_overflow(ell_max):
+    D = np.empty(sf.WignerDsize(0, ell_max), dtype=complex)
+    wigner = sf.Wigner(ell_max)
+
     # Test |Ra|=1e-10
     R = quaternionic.array(1.e-10, 1, 0, 0).normalized
-    assert np.all(np.isfinite(sf.Wigner_D_element(R, LMpM)))
+    assert np.all(np.isfinite(wigner.D(R, out=D)))
+
     # Test |Rb|=1e-10
     R = quaternionic.array(1, 1.e-10, 0, 0).normalized
-    assert np.all(np.isfinite(sf.Wigner_D_element(R, LMpM)))
+    assert np.all(np.isfinite(wigner.D(R, out=D)))
 
 
 def slow_Wignerd(beta, ell, mp, m):
@@ -201,77 +258,18 @@ def slow_Wigner_D_element(alpha, beta, gamma, ell, mp, m):
 
 
 @slow
-def test_Wigner_D_element_values(special_angles, ell_max):
-    LMpM = sf.LMpM_range_half_integer(0, ell_max // 2)
-    # Compare with more explicit forms given in Euler angles
+def test_Wigner_D_values(special_angles, ell_max, eps):
+    D = np.empty(sf.WignerDsize(0, ell_max), dtype=complex)
+    wigner = sf.Wigner(ell_max)
+    ell_mp_m = sf.WignerDrange(0, ell_max)
+    ϵ = 5 * ell_max**6 * eps
+
     print("")
     for alpha in special_angles:
-        print("\talpha={0}".format(alpha))  # Need to show some progress to Travis
+        print("\talpha={0}".format(alpha))  # Need to show some progress for CI
         for beta in special_angles:
             print("\t\tbeta={0}".format(beta))
             for gamma in special_angles:
-                a = np.conjugate(np.array([slow_Wigner_D_element(alpha, beta, gamma, ell, mp, m) for ell,mp,m in LMpM]))
-                b = sf.Wigner_D_element(quaternionic.array.from_euler_angles(alpha, beta, gamma), LMpM)
-                # if not np.allclose(a, b,
-                #     atol=ell_max ** 6 * precision_Wigner_D_element,
-                #     rtol=ell_max ** 6 * precision_Wigner_D_element):
-                #     for i in range(min(a.shape[0], 100)):
-                #         print(LMpM[i], "\t", abs(a[i]-b[i]), "\t\t", a[i], "\t", b[i])
-                assert np.allclose(a, b,
-                    atol=ell_max ** 6 * precision_Wigner_D_element,
-                    rtol=ell_max ** 6 * precision_Wigner_D_element)
-
-
-@slow
-def test_Wigner_D_matrix(Rs, ell_max):
-    for l_min in [0, 1, 2, ell_max // 2, ell_max - 1]:
-        print("")
-        for l_max in range(l_min + 1, ell_max + 1):
-            print("\tWorking on (l_min,l_max)=({0},{1})".format(l_min, l_max))
-            LMpM = sf.LMpM_range(l_min, l_max)
-            for R in Rs:
-                elements = sf.Wigner_D_element(R, LMpM)
-                matrix = np.empty(LMpM.shape[0], dtype=complex)
-                Rspinor = R.two_spinor
-                sf._Wigner_D_matrices(Rspinor.a, Rspinor.b, l_min, l_max, matrix)
-                assert np.allclose(elements, matrix,
-                                   atol=1e3 * l_max * ell_max * precision_Wigner_D_element,
-                                   rtol=1e3 * l_max * ell_max * precision_Wigner_D_element)
-
-
-@slow
-def test_Wigner_D_input_types(Rs, special_angles, ell_max):
-    LMpM = sf.LMpM_range(0, ell_max // 2)
-    # Compare with more explicit forms given in Euler angles
-    print("")
-    for alpha in special_angles:
-        print("\talpha={0}".format(alpha))  # Need to show some progress to Travis
-        for beta in special_angles:
-            for gamma in special_angles:
-                a = sf.Wigner_D_element(alpha, beta, gamma, LMpM)
-                b = sf.Wigner_D_element(quaternionic.array.from_euler_angles(alpha, beta, gamma), LMpM)
-                assert np.allclose(a, b,
-                                   atol=ell_max ** 6 * precision_Wigner_D_element,
-                                   rtol=ell_max ** 6 * precision_Wigner_D_element)
-    for R in Rs:
-        a = sf.Wigner_D_element(R, LMpM)
-        Rspinor = R.two_spinor
-        b = sf.Wigner_D_element(Rspinor.a, Rspinor.b, LMpM)
-        assert np.allclose(a, b,
-                           atol=ell_max ** 6 * precision_Wigner_D_element,
-                           rtol=ell_max ** 6 * precision_Wigner_D_element)
-
-
-def test_Wigner_D_signatures(Rs):
-    """There are two ways to call the WignerD function: with an array of Rs, or with an array of (ell,mp,m) values.
-    This test ensures that the results are the same in both cases."""
-    # from spherical.WignerD import _Wigner_D_elements
-    ell_max = 6
-    ell_mp_m = sf.LMpM_range(0, ell_max)
-    Ds1 = np.zeros((Rs.size//4, ell_mp_m.shape[0]), dtype=np.complex)
-    Ds2 = np.zeros_like(Ds1)
-    for i, R in enumerate(Rs):
-        Ds1[i, :] = sf.Wigner_D_element(R, ell_mp_m)
-    for i, (ell, mp, m) in enumerate(ell_mp_m):
-        Ds2[:, i] = sf.Wigner_D_element(Rs, ell, mp, m)
-    assert np.allclose(Ds1, Ds2, rtol=3e-15, atol=3e-15)
+                a = np.conjugate(np.array([slow_Wigner_D_element(alpha, beta, gamma, ell, mp, m) for ell,mp,m in ell_mp_m]))
+                b = wigner.D(quaternionic.array.from_euler_angles(alpha, beta, gamma), out=D)
+                assert np.allclose(a, b, rtol=ϵ, atol=ϵ)
