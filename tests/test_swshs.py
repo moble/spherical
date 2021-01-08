483c9afb8bb495434bd3e4b59fdf12e3def30e5c
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-01-08 15:57:53 -0500

    Prepare to remove SWSHs

--- tests/test_swshs.py
+++ tests/test_swshs.py
@@ -14,55 +14,6 @@ slow = pytest.mark.slow
 precision_SWSH = 2.e-15
 
 
-def test_SWSH_WignerD_expression(special_angles, ell_max):
-    for iota in special_angles:
-        for phi in special_angles:
-            for ell in range(ell_max + 1):
-                for s in range(-ell, ell + 1):
-                    R = quaternionic.array.from_euler_angles(phi, iota, 0)
-                    LM = np.array([[ell, m] for m in range(-ell, ell + 1)])
-                    Y = sf.SWSH(R, s, LM)
-                    LMS = np.array([[ell, m, -s] for m in range(-ell, ell + 1)])
-                    Rs = R.two_spinor
-                    D = (-1.) ** (s) * math.sqrt((2 * ell + 1) / (4 * np.pi)) * sf.Wigner_D_element(Rs.a, Rs.b, LMS)
-                    assert np.allclose(Y, D, atol=ell ** 6 * precision_SWSH, rtol=ell ** 6 * precision_SWSH)
-
-
-@slow
-def test_SWSH_spin_behavior(Rs, special_angles, ell_max):
-    # We expect that the SWSHs behave according to
-    # sYlm( R * exp(gamma*z/2) ) = sYlm(R) * exp(-1j*s*gamma)
-    # See http://moble.github.io/spherical/SWSHs.html#fn:2
-    # for a more detailed explanation
-    # print("")
-    for i, R in enumerate(Rs):
-        # print("\t{0} of {1}: R = {2}".format(i, len(Rs), R))
-        for gamma in special_angles:
-            for ell in range(ell_max + 1):
-                for s in range(-ell, ell + 1):
-                    LM = np.array([[ell, m] for m in range(-ell, ell + 1)])
-                    Rgamma = R * quaternionic.array(math.cos(gamma / 2.), 0, 0, math.sin(gamma / 2.))
-                    sYlm1 = sf.SWSH(Rgamma, s, LM)
-                    sYlm2 = sf.SWSH(R, s, LM) * cmath.exp(-1j * s * gamma)
-                    # print(R, gamma, ell, s, np.max(np.abs(sYlm1-sYlm2)))
-                    assert np.allclose(sYlm1, sYlm2, atol=ell ** 6 * precision_SWSH, rtol=ell ** 6 * precision_SWSH)
-
-
-@slow
-def test_SWSH_conjugation(special_angles, ell_max):
-    # {s}Y{l,m}.conjugate() = (-1.)**(s+m) {-s}Y{l,-m}
-    indices1 = sf.LM_range(0, ell_max)
-    indices2 = np.array([[ell, -m] for ell, m in indices1])
-    neg1_to_m = np.array([(-1.)**m for ell, m in indices1])
-    for iota in special_angles:
-        for phi in special_angles:
-            R = quaternionic.array.from_spherical_coordinates(iota, phi)
-            for s in range(1-ell_max, ell_max):
-                assert np.allclose(sf.SWSH(R, s, indices1),
-                                   (-1.)**s * neg1_to_m * np.conjugate(sf.SWSH(R, -s, indices2)),
-                                   atol=1e-15, rtol=1e-15)
-
-
 @slow
 def test_SWSH_grid(special_angles, ell_max_slow):
     ell_max = ell_max_slow
