fd53336a1e6536bc7a3c50aa922a2f5b16e625ba
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-01-08 16:39:10 -0500

    Update to current signatures

--- tests/test_utilities_mode_conversions.py
+++ tests/test_utilities_mode_conversions.py
@@ -10,20 +10,26 @@ import spherical as sf
 
 
 def test_constant_as_ell_0_mode(special_angles):
-    indices = np.array([[0, 0]])
+    ell_max = 1
+    wigner = sf.Wigner(ell_max)
     np.random.seed(123)
     for imaginary_part in [0.0, 1.0j]:  # Test both real and imaginary constants
-        for rep in range(1000):
+        for _ in range(1000):
             constant = np.random.uniform(-1, 1) + imaginary_part * np.random.uniform(-1, 1)
             const_ell_m = sf.constant_as_ell_0_mode(constant)
             assert abs(constant - sf.constant_from_ell_0_mode(const_ell_m)) < 1e-15
             for theta in special_angles:
                 for phi in special_angles:
-                    dot = np.dot(const_ell_m, sf.SWSH(quaternionic.array.from_spherical_coordinates(theta, phi), 0, indices))
-                    assert abs(constant - dot) < 1e-15
+                    Y = wigner.sYlm(0, quaternionic.array.from_spherical_coordinates(theta, phi))
+                    dot = np.dot(const_ell_m, Y[0])
+                    assert abs(constant - dot) < 1e-15, imaginary_part
 
 
 def test_vector_as_ell_1_modes(special_angles):
+    ell_min = 1
+    ell_max = 1
+    wigner = sf.Wigner(ell_max, ell_min=ell_min)
+
     indices = np.array([[1, -1], [1, 0], [1, 1]])
 
     def nhat(theta, phi):
@@ -32,12 +38,12 @@ def test_vector_as_ell_1_modes(special_angles):
                          math.cos(theta)])
 
     np.random.seed(123)
-    for rep in range(1000):
+    for _ in range(1000):
         vector = np.random.uniform(-1, 1, size=(3,))
         vec_ell_m = sf.vector_as_ell_1_modes(vector)
         assert np.allclose(vector, sf.vector_from_ell_1_modes(vec_ell_m), atol=1.0e-16, rtol=1.0e-15)
         for theta in special_angles:
             for phi in special_angles:
                 dot1 = np.dot(vector, nhat(theta, phi))
-                dot2 = np.dot(vec_ell_m, sf.SWSH(quaternionic.array.from_spherical_coordinates(theta, phi), 0, indices)).real
+                dot2 = np.dot(vec_ell_m, wigner.sYlm(0, quaternionic.array.from_spherical_coordinates(theta, phi))).real
                 assert abs(dot1 - dot2) < 1e-15
