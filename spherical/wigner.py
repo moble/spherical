Fix Yindex argument order; accept multi-dim quaternions in D and sYlm (#10)
* Update poetry
* Docstring formatting
* Fix Yindex argument order; closes #9
* Make scipy optional
* Generalize sYlm to accept multi-dimensional quaternions
* Test against scipy sph_harm
* Generalize Wigner.D to accept multi-dimensional quaternions
* Remove @slow marks for tests that aren't really slow

spherical/wigner.py
@@ -183,7 +183,7 @@ class Wigner:
 
             [
                 H(ℓ, mp, m)
-                for ℓ in range(ell_min, ell_max+1)
+                for ℓ in range(ell_max+1)
                 for mp in range(-min(ℓ, mp_max), min(ℓ, mp_max)+1)
                 for m in range(abs(mp), ℓ+1)
             ]
@@ -291,7 +291,7 @@ class Wigner:
             ]
 
         """
-        return Yindex(self.ell_min, ell, m)
+        return Yindex(ell, m, self.ell_min)
 
     def H(self, expiβ, Hwedge, Hv, Hextra):
         """Compute a quarter of the H matrix
@@ -469,6 +469,12 @@ class Wigner:
             raise ValueError(
                 f"Cannot compute full 𝔇 matrix up to ell_max={self.ell_max} if mp_max is only {self.mp_max}"
             )
+        if out is not None and out.size != (self.Dsize * R.size // 4):
+            raise ValueError(
+                f"Given output array has size {out.size}; it should be {self.Dsize * R.size // 4}"
+            )
+        if out is not None and out.dtype != np.complex:
+            raise ValueError(f"Given output array has dtype {out.dtype}; it should be complex")
 
         if workspace is not None:
             Hwedge, Hv, Hextra, zₐpowers, zᵧpowers, z = self._split_workspace(workspace)
@@ -477,13 +483,22 @@ class Wigner:
                 self.Hwedge, self.Hv, self.Hextra, self.zₐpowers, self.zᵧpowers, self.z
             )
 
-        to_euler_phases(R, z)
-        Hwedge = self.H(z[1], Hwedge, Hv, Hextra)
-        𝔇 = out if out is not None else np.zeros(self.Dsize, dtype=complex)
-        _complex_powers(z[0:1], self.ell_max, zₐpowers)
-        _complex_powers(z[2:3], self.ell_max, zᵧpowers)
-        _fill_wigner_D(self.ell_min, self.ell_max, self.mp_max, 𝔇, Hwedge, zₐpowers[0], zᵧpowers[0])
-        return 𝔇
+        quaternions = quaternionic.array(R).ndarray.reshape((-1, 4))
+        function_values = (
+            out.reshape(quaternions.shape[0], self.Dsize)
+            if out is not None
+            else np.zeros(quaternions.shape[:-1] + (self.Dsize,), dtype=complex)
+        )
+
+        # Loop over all input quaternions
+        for i_R in range(quaternions.shape[0]):
+            to_euler_phases(quaternions[i_R], z)
+            Hwedge = self.H(z[1], Hwedge, Hv, Hextra)
+            𝔇 = function_values[i_R]
+            _complex_powers(z[0:1], self.ell_max, zₐpowers)
+            _complex_powers(z[2:3], self.ell_max, zᵧpowers)
+            _fill_wigner_D(self.ell_min, self.ell_max, self.mp_max, 𝔇, Hwedge, zₐpowers[0], zᵧpowers[0])
+        return function_values.reshape(R.shape[:-1] + (self.Dsize,))
 
     def sYlm(self, s, R, out=None, workspace=None):
         """Evaluate (possibly spin-weighted) spherical harmonic
@@ -543,10 +558,12 @@ class Wigner:
                 f"This object has mp_max={self.mp_max}, which is not "
                 f"sufficient to compute sYlm values for spin weight s={s}"
             )
-        if out is not None and out.shape != (self.Ysize,):
+        if out is not None and out.size != (self.Ysize * R.size // 4):
             raise ValueError(
-                f"Given output array has shape {out.shape}; it should be {(self.Ysize,)}"
+                f"Given output array has size {out.size}; it should be {self.Ysize * R.size // 4}"
             )
+        if out is not None and out.dtype != np.complex:
+            raise ValueError(f"Given output array has dtype {out.dtype}; it should be complex")
 
         if workspace is not None:
             Hwedge, Hv, Hextra, zₐpowers, zᵧpowers, z = self._split_workspace(workspace)
@@ -555,14 +572,23 @@ class Wigner:
                 self.Hwedge, self.Hv, self.Hextra, self.zₐpowers, self.zᵧpowers, self.z
             )
 
-        to_euler_phases(R, z)
+        quaternions = quaternionic.array(R).ndarray.reshape((-1, 4))
+        function_values = (
+            out.reshape(quaternions.shape[0], self.Ysize)
+            if out is not None
+            else np.zeros(quaternions.shape[:-1] + (self.Ysize,), dtype=complex)
+        )
 
-        Hwedge = self.H(z[1], Hwedge, Hv, Hextra)
-        Y = out if out is not None else np.zeros(self.Ysize, dtype=complex)
-        _complex_powers(z[0:1], self.ell_max, zₐpowers)
-        zᵧpower = z[2]**abs(s)
-        _fill_sYlm(self.ell_min, self.ell_max, self.mp_max, s, Y, Hwedge, zₐpowers[0], zᵧpower)
-        return Y
+        # Loop over all input quaternions
+        for i_R in range(quaternions.shape[0]):
+            to_euler_phases(quaternions[i_R], z)
+            Hwedge = self.H(z[1], Hwedge, Hv, Hextra)
+            Y = function_values[i_R]
+            _complex_powers(z[0:1], self.ell_max, zₐpowers)
+            zᵧpower = z[2]**abs(s)
+            _fill_sYlm(self.ell_min, self.ell_max, self.mp_max, s, Y, Hwedge, zₐpowers[0], zᵧpower)
+
+        return function_values.reshape(R.shape[:-1] + (self.Ysize,))
 
     def rotate(self, modes, R, out=None, workspace=None, horner=False):
         """Rotate Modes object
@@ -647,7 +673,6 @@ class Wigner:
             **modes._metadata
         )
 
-
     def evaluate(self, modes, R, out=None, workspace=None, horner=False):
         """Evaluate Modes object as function of rotations
 
@@ -669,10 +694,10 @@ class Wigner:
             present, this object's default workspace will be used.  Note that it is not
             safe to use the same workspace on multiple threads.
         horner : bool, optional
-            If False (the default), rotation will be done using matrix multiplication
-            with Wigner's 𝔇 — which will typically use BLAS, and thus be as fast as
-            possible.  If True, the result will be built up using Horner form, which
-            should be more accurate, but may be significantly slower.
+            If False (the default), evaluation will be done using vector multiplication
+            with sYlm — which will typically use BLAS, and thus be as fast as possible.
+            If True, the result will be built up using Horner form, which should be
+            more accurate, but may be significantly slower.
 
         Returns
         -------

tests/test_utilities_indexing.py
@@ -41,7 +41,6 @@ def test_WignerHsize(ell_max):
     # print(f"Total sizes checked: {k:_}")
 
 
-@slow
 def test_WignerHindex(ell_max_slow):
     # k = 0
     def fold_H_indices(ell, mp, m):
@@ -76,7 +75,6 @@ def test_WignerHindex(ell_max_slow):
     # print(f"\nTotal indices checked: {k:_}")
 
 
-@slow
 def test_WignerDrange(ell_max_slow):
     def r(ell_min, mp_max, ell_max):
         return [
@@ -247,8 +245,8 @@ def test_LM_total_size(ell_max):
             assert sf.LM_index(l_max + 1, -(l_max + 1), l_min) == sf.LM_total_size(l_min, l_max)
 
 
-def test_LMpM_range(ell_max):
-    for l_max in range(ell_max + 1):
+def test_LMpM_range(ell_max_slow):
+    for l_max in range(ell_max_slow + 1):
         assert np.array_equal(sf.LMpM_range(l_max, l_max),
                               np.array([[l_max, mp, m]
                                         for mp in range(-l_max, l_max + 1)
@@ -261,7 +259,6 @@ def test_LMpM_range(ell_max):
                                             for m in range(-ell, ell + 1)]))
 
 
-@slow
 def test_LMpM_index(ell_max_slow):
     for ell_min in range(ell_max_slow + 1):
         LMpM = sf.LMpM_range(ell_min, ell_max_slow)

tests/test_wigner_sYlm.py
@@ -10,6 +10,8 @@ import quaternionic
 import spherical as sf
 import pytest
 
+from .conftest import requires_scipy
+
 slow = pytest.mark.slow
 
 precision_SWSH = 2.e-15
@@ -98,7 +100,74 @@ def test_sYlm_vs_NINJA(special_angles, ell_max_slow, eps):
             assert np.allclose(Y1, Y2, rtol=ϵ, atol=ϵ)
 
 
-@slow
+@requires_scipy
+def test_sYlm_vs_scipy(special_angles, ell_max, eps):
+    from scipy.special import sph_harm
+    ϵ = 2 * ell_max * eps
+    wigner = sf.Wigner(ell_max)
+
+    # Test one quaternion at a time
+    # print()
+    for theta in np.arange(0, 1 * np.pi + 0.1, np.pi / 8.):
+        for phi in np.arange(0, 2 * np.pi + 0.1, np.pi / 8.):
+            R = quaternionic.array.from_spherical_coordinates(theta, phi)
+            # Note that sph_harm names its arguments (theta, phi) in that order, but swaps the
+            # meaning of theta and phi, so we have to swap the order here.
+            Y1 = np.array([
+                sph_harm(m, ell, phi, theta)
+                for ell in range(ell_max+1)
+                for m in range(-ell, ell+1)
+            ])
+            Y2 = wigner.sYlm(0, R)
+            # print(Y1.shape, Y2.shape, theta, phi)
+            assert np.allclose(Y1, Y2, rtol=ϵ, atol=ϵ)
+
+    # Test N quaternions at a time
+    theta_phi = [
+        [theta, phi]
+        for theta in np.arange(0, 1 * np.pi + 0.1, np.pi / 8.)
+        for phi in np.arange(0, 2 * np.pi + 0.1, np.pi / 8.)
+    ]
+    R = quaternionic.array.from_spherical_coordinates(theta_phi)
+    Y1 = np.array([
+        [
+            sph_harm(m, ell, phi, theta)
+            for ell in range(ell_max+1)
+            for m in range(-ell, ell+1)
+        ]
+        for theta, phi in theta_phi
+    ])
+    Y2 = wigner.sYlm(0, R)
+    # print()
+    # print(Y1.shape, Y2.shape)
+    assert np.allclose(Y1, Y2, rtol=ϵ, atol=ϵ)
+
+    # Test NxM quaternions at a time
+    theta_phi = [
+        [
+            [theta, phi]
+            for theta in np.arange(0, 1 * np.pi + 0.1, np.pi / 8.)
+        ]
+        for phi in np.arange(0, 2 * np.pi + 0.1, np.pi / 8.)
+    ]
+    R = quaternionic.array.from_spherical_coordinates(theta_phi)
+    Y1 = np.array([
+        [
+            [
+                sph_harm(m, ell, phi, theta)
+                for ell in range(ell_max+1)
+                for m in range(-ell, ell+1)
+            ]
+            for theta, phi in tp
+        ]
+        for tp in theta_phi
+    ])
+    Y2 = wigner.sYlm(0, R)
+    # print()
+    # print(Y1.shape, Y2.shape)
+    assert np.allclose(Y1, Y2, rtol=ϵ, atol=ϵ)
+
+
 def test_sYlm_WignerD_expression(special_angles, ell_max_slow, eps):
     # ₛYₗₘ(R) = (-1)ˢ √((2ℓ+1)/(4π)) 𝔇ˡₘ₋ₛ(R)
     #        = (-1)ˢ √((2ℓ+1)/(4π)) 𝔇̄ˡ₋ₛₘ(R̄)
@@ -120,7 +189,6 @@ def test_sYlm_WignerD_expression(special_angles, ell_max_slow, eps):
                     assert np.allclose(Y_ℓ, Y_D, atol=ϵ, rtol=ϵ)
 
 
-@slow
 def test_sYlm_spin_behavior(Rs, special_angles, ell_max_slow, eps):
     # We expect that the SWSHs behave according to
     # sYlm( R * exp(gamma*z/2) ) = sYlm(R) * exp(-1j*s*gamma)
@@ -139,7 +207,6 @@ def test_sYlm_spin_behavior(Rs, special_angles, ell_max_slow, eps):
                 assert np.allclose(Y1, Y2, atol=ϵ, rtol=ϵ)
 
 
-@slow
 def test_sYlm_conjugation(special_angles, ell_max_slow, eps):
     # {s}Y{l,m}.conjugate() = (-1.)**(s+m) {-s}Y{l,-m}
     ϵ = 2 * ell_max_slow * eps